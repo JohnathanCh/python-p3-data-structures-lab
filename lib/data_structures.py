5ddb6040992ef6963108d4aa2976a74fa8ff6dda
JohnathanCh <johnchriest@gmail.com>
2023-01-06 18:25:11 -0800

    done

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -1,3 +1,5 @@
+import pdb
+
 spicy_foods = [
     {
         "name": "Green Curry",
@@ -17,22 +19,34 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    return [dict["name"] for dict in spicy_foods]
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [dict for dict in spicy_foods if dict["heat_level"] > 5]
 
 def print_spicy_foods(spicy_foods):
-    pass
+    stuff = [f"{dict['name']} ({dict['cuisine']}) | Heat Level: {'🌶' * dict['heat_level']}" for dict in spicy_foods]
+    
+    for food in stuff:
+        print(food)
+        # print(f"{dict['name']} ({dict['cuisine']}) | Heat Level: {'🌶' * dict['heat_level']}")
+    # pdb.set_trace()
+    return stuff
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+       if food["cuisine"] == cuisine:
+            return food 
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    new_foods = get_spiciest_foods(spicy_foods)
+    print_spicy_foods(new_foods)
+    
 
 def get_average_heat_level(spicy_foods):
-    pass
+    nums = [food["heat_level"] for food in spicy_foods]
+    return sum(nums)/len(nums)
